7306dfe5ed80d64c2f773613f9d5a05f55285a4b
Hong Myeng Jin <84975902+HongMyengJin@users.noreply.github.com>
2024-01-02 21:59:59 +0900

    [level unrated] Title: 문자열 섞기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.cpp
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.cpp
@@ -5,13 +5,11 @@ using namespace std;
 
 string solution(string str1, string str2) {
     string answer = "";
-    int istringLength = str1.length() + str2.length();
-    answer.resize(istringLength);
     for(int i = 0; i < str1.length(); i++)
-        answer[i * 2] = str1[i];
-    
-    for(int i = 0; i < str2.length(); i++)
-        answer[i * 2 + 1] = str2[i];
+    {
+        answer.push_back(str1[i]);
+        answer.push_back(str2[i]);
+    }
     
     return answer;
 }
