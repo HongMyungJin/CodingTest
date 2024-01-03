f5804f8eb3d9d44149fafff30e9ea1aa9c6299c6
Hong Myeng Jin <84975902+HongMyengJin@users.noreply.github.com>
2024-01-03 23:37:21 +0900

    [level unrated] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.cpp
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.cpp
@@ -4,14 +4,5 @@
 using namespace std;
 
 int solution(int a, int b) {
-    int answer = 0;
-
-    int answer0 = stoi(to_string(a) + to_string(b));
-    int answer1 = stoi(to_string(b) + to_string(a));
-    if (answer0 > answer1)
-        answer = answer0;
-    else
-        answer = answer1;
-
-    return answer;
+    return max(stoi((to_string(a) + to_string(b))), stoi((to_string(b) + to_string(a))));
 }
