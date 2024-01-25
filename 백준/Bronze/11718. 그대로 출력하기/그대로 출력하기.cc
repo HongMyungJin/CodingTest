0509ba01666cd61388e796085c4b74b05101c2ce
Hong Myeng Jin <84975902+HongMyengJin@users.noreply.github.com>
2024-01-25 21:09:44 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 8 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+int main()
+{
+	vector<string> Words;
+	string Word;
+	while (getline(cin, Word))
+		cout << Word << endl;
+
+
+}
