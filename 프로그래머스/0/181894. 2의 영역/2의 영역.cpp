97a60a556a3a9aa8eed320dab7dc7f4e8c793ae6
Hong Myeng Jin <84975902+HongMyengJin@users.noreply.github.com>
2024-01-25 22:25:12 +0900

    [level 0] Title: 2의 영역, Time: 20.04 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.cpp
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.cpp
@@ -6,13 +6,14 @@ using namespace std;
 vector<int> solution(vector<int> arr) {
 	vector<int> answer;
 	int N = std::count(arr.begin(), arr.end(), 2);
-    if(N == 0)
+    
+    vector<int>::iterator i = std::find(arr.begin(), arr.end(), 2);
+    if(i == arr.end())
     {
         answer.emplace_back(-1);
         return answer;
     }
-    
-	for (vector<int>::iterator i = std::find(arr.begin(), arr.end(), 2); N > 0; i++)
+	for (; N > 0; i++)
 	{
 		if (*i == 2) N--;
 		answer.emplace_back(*i);
