7ca967733a057d7b2af7e63541a7c79db912b52d
Hong Myeng Jin <84975902+HongMyengJin@users.noreply.github.com>
2024-01-30 23:25:51 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 4.45 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int total = 0;
+    for(int i : num_list)
+    {
+        while(i != 1)
+        {
+            if(i % 2 == 0)
+                i /= 2;
+            else
+                i = (i - 1) / 2;
+            total++;
+        }
+    }
+    return total;
+}
