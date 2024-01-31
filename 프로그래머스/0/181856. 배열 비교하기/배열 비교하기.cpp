364b61fbb5bd2effe0550d3d01fa9234f6d6d97a
Hong Myeng Jin <84975902+HongMyengJin@users.noreply.github.com>
2024-02-01 00:03:51 +0900

    [level 0] Title: 배열 비교하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.cpp
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.cpp
@@ -8,7 +8,9 @@ int solution(vector<int> arr1, vector<int> arr2) {
     if(arr1.size() != arr2.size())
         return arr1.size() > arr2.size() ? 1 : -1;
     else
-        if((accumulate(arr1.begin(), arr1.end(), 0) - accumulate(arr2.begin(), arr2.end(), 0)) == 0)
-            return 0;
-        return accumulate(arr1.begin(), arr1.end(), 0) > accumulate(arr2.begin(), arr2.end(), 0) ? 1 : -1;
+    {
+        int sum1 = accumulate(arr1.begin(), arr1.end(), 0);
+        int sum2 = accumulate(arr2.begin(), arr2.end(), 0);
+        return sum1 == sum2 ? 0 : (sum1 > sum2 ? 1 : -1);
+    }
 }
