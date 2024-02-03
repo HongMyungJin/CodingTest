e3e008e3b391f17d2180dd02498c4e4a57c51b8f
Hong Myeng Jin <84975902+HongMyengJin@users.noreply.github.com>
2024-02-03 22:01:11 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    
+    stringstream ss(my_string);
+    string word;
+    while(getline(ss, word, ' '))
+    {
+        if(word != "")
+            answer.emplace_back(word);
+    }
+    return answer;
+}
