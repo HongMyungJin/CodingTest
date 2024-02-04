d344ba604b82ee0752ea119d6cb6d92b4a4b9a1d
Hong Myeng Jin <84975902+HongMyengJin@users.noreply.github.com>
2024-02-04 22:38:15 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cpp
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cpp
@@ -4,11 +4,9 @@
 using namespace std;
 
 int solution(vector<int> date1, vector<int> date2) {
-    for(int i = 0; i < 3;)
+    for(int i = 0; i < 3; i++)
     {
-        if(date1[i] == date2[i])
-            i++;
-        else
+        if(date1[i] != date2[i])
             return date1[i] < date2[i] ? 1 : 0;
     }
     
