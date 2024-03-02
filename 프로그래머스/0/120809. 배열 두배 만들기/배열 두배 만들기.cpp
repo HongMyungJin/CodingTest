1b5ae4a63ea0e052652089c3cfbee18c7e5516f5
Hong Myeng Jin <84975902+HongMyengJin@users.noreply.github.com>
2024-03-02 23:52:11 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.15 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    transform(numbers.begin(), numbers.end(), numbers.begin(), [](int n) {return n * 2;});
+    return numbers;
+}
