879e784a9a38f3c9b4b7ac7421f6a75b2ffb726a
Hong Myeng Jin <84975902+HongMyengJin@users.noreply.github.com>
2024-03-02 23:59:26 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(string my_string) {
+    vector<int> answer;
+    for(char i : my_string)
+    {
+        if(i >= '0' && i <= '9')
+            answer.push_back(i - '0');
+    }
+    sort(answer.begin(), answer.end());
+    return answer;
+}
