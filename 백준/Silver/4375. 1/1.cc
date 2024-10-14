cdf05bf6f5b3d73731dfeca0f5412297bb8001ec
Hjj0731 <84975902+HongMyengJin@users.noreply.github.com>
2024-10-14 16:08:33 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.cc
+++ 백준/Silver/4375. 1/1.cc
@@ -1,21 +1,18 @@
 #include<iostream>
 using namespace std;
-/*
-(a * b) % n  = (a % n) * (b % n)
-*/
+
 int DigitData(int n)
 {
 	int data = 1;
-
 	int N = 1;
+    
 	while (1)
 	{
 		if (data % n == 0)
 			return N;
 		else
 		{
-			data = data * 10 + 1;
-			data %= n;
+			data = (data * 10 + 1) % n;
 			N++;
 		}
 	}
@@ -26,9 +23,7 @@ int main()
 {
 	int n = 0;
 	while (scanf("%d", &n) != EOF)
-	{
 		cout << DigitData(n) << endl;
-	}
 	
 	return 0;
 }
