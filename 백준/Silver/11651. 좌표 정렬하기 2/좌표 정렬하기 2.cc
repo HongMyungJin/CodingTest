4689971d65a7de6cd9cafa0fb1e940a36dce75de
Hjj0731 <84975902+HongMyengJin@users.noreply.github.com>
2025-04-17 06:39:02 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 100 ms, Memory: 3592 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+struct POINT
+{
+	POINT() {}
+	POINT(int X, int Y) {
+		x = X;
+		y = Y;
+	}
+
+	int x;
+	int y;
+};
+
+std::vector<POINT> SortN(std::vector<POINT>& values)
+{
+	std::sort(values.begin(), values.end(), [](POINT a, POINT b) 
+		{    if (a.y != b.y) return a.y < b.y; return a.x < b.x; }
+	);
+	return values;
+}
+
+int main()
+{
+	int N, x, y;
+	std::cin >> N;
+	std::vector<POINT> value(N);
+	for(int i = 0; i < N; i++){ 
+		std::cin >> x >> y;
+		value[i] = POINT(x, y);
+	}
+		
+
+	for (POINT point : SortN(value))
+		std::cout << point.x << " " << point.y << "\n";
+
+	return 0;
+}
