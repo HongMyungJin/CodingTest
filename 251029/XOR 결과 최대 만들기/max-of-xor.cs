1d5d0948aedc1c623002c2819b9f14df8e5ac592
HMJ <84975902+HongMyungJin@users.noreply.github.com>
2025-10-29 17:43:52 +0900

    Title: XOR 결과 최대 만들기, Time: 76ms, Memory: 5MB, Status: Wrong Answer - Codetree

---
+++ 251029/XOR 결과 최대 만들기/max-of-xor.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+
+public class Codetree
+{
+    private static int N;
+    private static int M;
+    private static int Max;
+    private static List<int> inputs;
+    private static List<int> outputs;
+
+    private static int XORSolve()
+    {
+        int xorValue = outputs[0];
+        for (int i = 1; i < M; i++)
+        {
+            xorValue ^= outputs[i];
+        }
+
+        return xorValue;
+    }
+
+    private static void Solve(int cur)
+    {
+        if (cur == M)
+        {
+            Max = Math.Max(XORSolve(), Max);
+            return;
+        }
+
+        // 애초에 넣을 때 큰 것부터..
+        for (int i = 0; i < N; i++)
+        {
+            // 이전에 넣은 값이 현재 넣을려는 값보다 크면 넘기기
+            if (outputs.Count >= 2 && outputs[outputs.Count - 1] >= inputs[i])
+            {
+                continue;
+            }
+
+            outputs.Add(inputs[i]);
+
+            Solve(cur + 1);
+
+            outputs.RemoveAt(outputs.Count - 1);
+        }
+    }
+    public static void Main()
+    {
+        // Please write your code here.
+        string[] values = Console.ReadLine().Split(' ');
+
+        inputs = new List<int>();
+        outputs = new List<int>();
+        N = int.Parse(values[0]);
+        M = int.Parse(values[1]);
+        Max = 0;
+        string[] valueLists = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i < N; i++)
+        {
+            inputs.Add(int.Parse(valueLists[i]));
+        }
+
+        Solve(0);
+
+        Console.WriteLine(Max);
+    }
+}
