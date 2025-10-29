82df2ef489702eeb03ff375e0f725b4983181066
HMJ <84975902+HongMyungJin@users.noreply.github.com>
2025-10-29 18:16:02 +0900

    Title: XOR 결과 최대 만들기, Time: 70ms, Memory: 5MB, Status: Passed - Codetree

--- 251029/XOR 결과 최대 만들기/max-of-xor.cs
+++ 251029/XOR 결과 최대 만들기/max-of-xor.cs
@@ -9,19 +9,7 @@ public class Codetree
     private static List<int> inputs;
     private static List<int> outputs;
 
-    private static bool CheckIncrease()
-    {
-        long value = outputs[0];
-        for (int i = 1; i < M; i++)
-        {
-            if (outputs[i - 1] >= outputs[i])
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
+    
     private static long XORSolve()
     {
         long xorValue = outputs[0];
@@ -33,29 +21,22 @@ public class Codetree
         return xorValue;
     }
 
-    private static void Solve(int cur)
+    private static void Solve(int cur, int start)
     {
         if (cur == M)
         {
-            if (CheckIncrease())
-            {
-                Max = Math.Max(XORSolve(), Max);
-            }
+            Max = Math.Max(XORSolve(), Max);
             return;
         }
 
-        // 애초에 넣을 때 큰 것부터..
-        for (int i = 0; i < N; i++)
-        {
-            // 이전에 넣은 값이 현재 넣을려는 값보다 크면 넘기기
-            if (outputs.Count >= 2 && outputs[outputs.Count - 1] >= inputs[i])
-            {
-                continue;
-            }
 
+        // 애초에 넣을 때 큰 것부터..
+        for (int i = start; i < N; i++)
+        {
+            // 이전에 넣은 값이 현재 넣을려는 값보다 크면 넘기
             outputs.Add(inputs[i]);
 
-            Solve(cur + 1);
+            Solve(cur + 1, i + 1);
 
             outputs.RemoveAt(outputs.Count - 1);
         }
@@ -77,7 +58,7 @@ public class Codetree
             inputs.Add(int.Parse(valueLists[i]));
         }
 
-        Solve(0);
+        Solve(0, 0);
 
         Console.WriteLine(Max);
     }
